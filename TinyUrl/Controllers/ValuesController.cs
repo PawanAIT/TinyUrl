0dd2f54674936babad7f6dd8e4e27c63d0f2599f
pawan kumar <pawkumar@microsoft.com>
2019-07-21 03:21:02 +0530

    build pipeline testing

--- TinyUrl/Controllers/ValuesController.cs
+++ TinyUrl/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -54,6 +55,11 @@ namespace TinyUrl.Controllers
                 }
                 return database.PutLongUrl(Shorturl: guid, Longurl: LongUrl);
             }
+        }
+        [HttpGet]
+        public IEnumerable<string> Show()
+        {
+            return new string[] { "Pawan", "Kumar" };
         }
     }
 }
